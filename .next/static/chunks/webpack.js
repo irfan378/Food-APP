05108272f2617c29ac619b8ca9ba8b994e990fd1
irfan378 <irfandominate@gmail.com>
2022-06-26 18:38:47 +0530

    product page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5368798da2ad724a"; }
+/******/ 		__webpack_require__.h = function() { return "88ff67a1a59979f5"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- component/Footer.tsx
+++ component/Footer.tsx
@@ -4,41 +4,41 @@ import Image from "next/image";
 
 const Footer: NextPage = () => {
   return (
-    <div className="h-[calc(100vh-100px)] bg-black flex ">
-      <div className="flex-[1] relative flex">
+    <div className="container2 h-[calc(100vh-100px)] bg-black flex ">
+      <div className="flex-[1] relative flex item">
         <Image src="/img/bg.png" objectFit="cover" layout="fill" alt="" />
       </div>
-      <div className="flex-[1] relative flex">
-        <div className="flex-[1]">
-          <h2>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, in?
+      <div className="flex-[2] p-14 justify-between relative flex item">
+        <div className="flex-[1] card px-2 py-5 ">
+          <h2 className="motto text-[#b09999]  py-5 font-medium  text-5xl">
+           Let us feel you different here.
           </h2>
         </div>
-        <div>
-          <h1>Our Restaurants</h1>
-          <p>
+        <div className="card flex-[1] px-0 py-5">
+          <h1 className="title text-3xl text-[#b7903c]">Our Restaurants</h1>
+          <p className="text text-[lightgrey] text-2xl py-5 font-thin">
             Srinagar <br /> Jammu and Kashmir <br />
             0123456789
           </p>
-          <p>
+          <p className="text text-[lightgrey] text-2xl py-5 font-thin">
             Srinagar <br /> Jammu and Kashmir <br />
             0123456789
           </p>
-          <p>
+          <p className="text text-[lightgrey] text-2xl py-5 font-thin">
             Srinagar <br /> Jammu and Kashmir <br />
             0123456789
           </p>
-          <p>
+          <p className="text text-[lightgrey] text-2xl py-5 font-thin">
             Srinagar <br /> Jammu and Kashmir <br />
             0123456789
           </p>
         </div>
-        <div className="flex-[1] relative flex">
-          <h1>Working Hours</h1>
-          <p>
+        <div className="card  flex-[1] px-0 py-5">
+          <h1 className="title text-3xl text-[#b7903c]">Working Hours</h1>
+          <p className="text text-[lightgrey] text-2xl py-5 font-thin">
             Monday - Friday <br /> 9:00 - 22:00
           </p>
-          <p>
+          <p className="text text-[lightgrey] text-2xl py-5 font-thin">
             Saturday - Sunday <br />
             12:00 - 24:00
           </p>
