856a08851a42d6c6e5844b63e69736ff6f7b0a44
irfan378 <irfandominate@gmail.com>
2022-07-04 18:41:46 +0530

    models

--- models/Product.ts
+++ models/Product.ts
@@ -1,1 +1,16 @@
-import { createSchema, Type, typedModel } from 'ts-mongoose';
+import { createSchema, Type, typedModel } from "ts-mongoose";
+
+const ProductSchema = createSchema(
+  {
+    title: Type.string({ required: true, maxlength: 60 }),
+    desc: Type.string({ required: true, maxlength: 200 }),
+    img: Type.string({ required: true }),
+    prices: Type.number({ required: true }),
+    extraOptions: Type.array().of({
+      text: Type.string({ required: true }),
+      price: Type.number({ required: true }),
+    }),
+  },
+  { timestamps: true }
+);
+export const Product = typedModel("Product", ProductSchema);
