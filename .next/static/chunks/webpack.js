ab7b21904f7bd8b39578deb2fe2b5fc8c24f87b0
irfan378 <irfandominate@gmail.com>
2022-07-19 09:38:25 +0530

    product page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "921eea438110a903"; }
+/******/ 		__webpack_require__.h = function() { return "20c53dfe335dfbc2"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
